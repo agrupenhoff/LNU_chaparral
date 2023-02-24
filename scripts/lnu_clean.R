a9ca962e84649dd49bc098a54044275634999a58
Ashley Grupenhoff <agrupenhoff@ucdavis.edu>
2023-02-23 16:26:47 -0800

    update regen model

--- scripts/lnu_clean.R
+++ scripts/lnu_clean.R
@@ -15,12 +15,15 @@ GroundCover_LNU <- read.csv("data/raw/GroundCover_LNU.csv")
 Mortality_LNU <- read.csv("data/raw/Mortality_LNU.csv")
 SpeciesList <- read.csv("data/raw/SpeciesList_jan23.csv")
 
+
 str(subplot_species_LNU)
 subplot_species_LNU$Q1 = as.numeric(subplot_species_LNU$Q1)
 subplot_species_LNU$Q2 = as.numeric(subplot_species_LNU$Q2)
 subplot_species_LNU$Q3 = as.numeric(subplot_species_LNU$Q3)
 subplot_species_LNU$Q4 = as.numeric(subplot_species_LNU$Q4)
 subplot_species_LNU$Q5 = as.numeric(subplot_species_LNU$Q5)
+subplot_species_LNU$seedling_resp = toupper(subplot_species_LNU$seedling_resp)
+subplot_species_LNU$cover_count_ht = toupper(subplot_species_LNU$cover_count_ht)
 
 #SEVERITY DATA
 severity_LNU_byplot <- severity_LNU %>% 
@@ -111,99 +114,101 @@ LNU_sub_nonnative_wide <- LNU_subplot_nonnative %>%
 write.csv(LNU_subplot_nonnative,"data/clean/LNU_subplot_nonnative_long.csv" )
 write.csv(LNU_sub_nonnative_wide,"data/clean/LNU_subplot_nonnative_wide.csv" )
 
-#SHRUB DENSITY
+#SHRUB DENSITY 
 
+#make data frame with every combination of plot x shrub spp combination
+sh_spp <- subplot_species_LNU %>% filter(Lifeform == "SH")
+unique(sh_spp$spp)
+nd <- expand_grid(PlotID =unique(subplot_species_LNU$PlotID),
+                  spp =unique(sh_spp$spp),
+                  year=unique(subplot_species_LNU$year))
+str(nd)
+nd$year <- as.numeric(nd$year)
 
+#Shrub den - avg and sum count
+str(shrub_density_long)
 shrub_density_long <- subplot_species_LNU %>% 
-  filter(cover_count_ht == "COUNT") %>% 
-  mutate(species_seed.resp = paste(spp, seedling_resp, sep ="_")) %>%   
+  filter(cover_count_ht == "COUNT",
+         seedling_resp == "SEEDLING") %>% 
   mutate(plot_year = paste(PlotID, year, sep="_")) %>%  
-  mutate(plot_spp = paste(plot_year, species_seed.resp, by=" ")) %>% 
+  mutate(plot_spp = paste(plot_year, spp, by=" ")) %>% 
   select(plot_spp, Q1, Q2, Q3, Q4, Q5) %>% 
   mutate_if(is.numeric, ~replace_na(., 0)) %>% 
   mutate(avg.count = (Q1+Q2+Q3+Q4+Q5)/5,
          sum.count = Q1+Q2+Q3+Q4+Q5) %>% 
   select(plot_spp, avg.count, sum.count) %>% 
-  separate(plot_spp, c("plot_year", "spp.seed.resp"), sep=" ") 
+  separate(plot_spp, c("plot_year", "spp"), sep=" ") %>% 
+  separate(plot_year, c("PlotID", "year"), sep="_") 
 
-shrub_density_wide <- shrub_density_long %>% 
-  pivot_wider(
-    names_from = "spp.seed.resp",
-    values_from = c("avg.count", "sum.count"),
-    values_fill = 0,
-    values_fn = sum
-  )  
-str(shrub_density_wide)
+shrub_density_long$year <- as.numeric(shrub_density_long$year)
+#now add to nd to account for 0s
+shrub_density_zeros <- left_join(nd, shrub_density_long, 
+                                 by=c("PlotID","spp","year"))
+shrub_density_final <- shrub_density_zeros %>% 
+  mutate_if(is.numeric, ~replace_na(., 0)) 
 
-shrub_density_long_final <- shrub_density_wide %>% 
-  pivot_longer(!plot_year, 
-               names_to = 'spp.seed.resp',
+write.csv(shrub_density_final, "data/clean/shrub_density_count.csv")
+
+#SHRUB DENSITY - proportion of presence 
+shrub_density_presence <- subplot_species_LNU %>% 
+  filter(cover_count_ht == "COUNT",
+         seedling_resp == "SEEDLING") %>% 
+  mutate(plot_year = paste(PlotID, year, sep="_")) %>%  
+  mutate(plot_spp = paste(plot_year, spp, by=" ")) %>% 
+  select(plot_spp, Q1, Q2, Q3, Q4, Q5) %>% 
+  mutate_if(is.numeric, ~replace_na(., 0)) %>% 
+  pivot_longer(!plot_spp, 
+               names_to = 'quad',
                values_to = 'density') %>% 
-  separate(plot_year, c("PlotID", "year"), sep="_") %>% 
-  separate(spp.seed.resp, c("avg.sum", "spp", "seed.resp"), sep="_") 
+  mutate(presence = case_when(
+    density == 0 ~ 0,
+    density > 0 ~ 1
+  )) %>% 
+  group_by(plot_spp) %>% 
+  dplyr::summarise(presence = sum(presence),
+            prop.presence = presence/5) %>% 
+  separate(plot_spp, c("plot_year", "spp"), sep=" ") %>% 
+  separate(plot_year, c("PlotID", "year"), sep="_")
 
+str(shrub_density_presence)
+shrub_density_presence$year <- as.numeric(shrub_density_presence$year)
+#now add to nd to account for 0s
+shrub_presence_zeros <- left_join(nd, shrub_density_presence, 
+                                 by=c("PlotID","spp","year"))
 
-write.csv(shrub_density_wide, "data/clean/LNU_subplot_shrub_density_wide.csv")
-write.csv(shrub_density_long_final, "data/clean/LNU_subplot_shrub_density_long.csv")
+shrub_presence_final <- shrub_presence_zeros %>% 
+  mutate_if(is.numeric, ~replace_na(., 0)) 
+write.csv(shrub_presence_final, "data/clean/shrub_density_presence.csv")
 
 #SHRUB COVER
 shrub_cover_long <- subplot_species_LNU %>% 
-  filter(cover_count_ht == "COVER") %>% 
-  mutate(species_seed.resp = paste(spp, seedling_resp, sep ="_")) %>% 
+  filter(cover_count_ht == "COVER",
+         Lifeform == "SH",
+         seedling_resp == "SEEDLING") %>% 
   mutate(plot_year = paste(PlotID, year, sep="_")) %>% 
-  mutate(plot_spp = paste(plot_year, species_seed.resp, by=" ")) %>% 
+  mutate(plot_spp = paste(plot_year, spp, by=" ")) %>% 
   select(plot_spp, Q1, Q2, Q3, Q4, Q5) %>% 
   mutate_if(is.numeric, ~replace_na(., 0)) %>% 
-  pivot_longer(!plot_spp, names_to = 'quad', values_to = 'cover') %>% 
-  separate(plot_spp, c("plot_year", "spp.seed.resp"), sep=" ") 
+  mutate(avg.cover = (Q1+Q2+Q3+Q4+Q5)/5,
+         sum.cover = Q1+Q2+Q3+Q4+Q5) %>% 
+  select(plot_spp, avg.cover, sum.cover) %>% 
+  separate(plot_spp, c("plot_year", "spp"), sep=" ") %>% 
+  separate(plot_year, c("PlotID", "year"), sep="_")
 
 str(shrub_cover_long)
+shrub_cover_long$year <- as.numeric(shrub_cover_long$year)
+#now add to nd to account for 0s
+shrub_cover_zeros <- left_join(nd, shrub_cover_long, 
+                                  by=c("PlotID","spp","year"))
 
-shrub_cover_plot <- shrub_cover_long %>% 
-  group_by(plot_year, spp.seed.resp) %>% 
-  dplyr::summarise(cover = mean(cover)) %>% 
-  separate(spp.seed.resp, c("spp", "seed.resprout"), sep="_") %>% 
-  separate(plot_year, c("PlotID", "year"), sep="_") 
+shrub_cover_final <- shrub_cover_zeros %>% 
+  mutate_if(is.numeric, ~replace_na(., 0)) 
 
-write.csv(shrub_cover_plot, "data/clean/LNU_subplot_shrub_cover.csv")
+write.csv(shrub_cover_final, "data/clean/shrub_seedling_cover.csv")
     
+shrub_seedling_data <- left_join(shrub_density_final, shrub_presence_final,
+                                 by=c("PlotID", "year", "spp"))
+shrub_seedling_data <- left_join(shrub_seedling_data, shrub_cover_final,
+                                 by=c("PlotID", "year", "spp"))
 
-#Total number of species
-TOT_species_LNU <- TOT_species_LNU %>% 
-  dplyr::rename(spp = Species) %>% 
-  distinct(PlotID, year, spp)
-
-TOT_species_LNU <- left_join(TOT_species_LNU, species.short,
-                                 by = "spp")
-
-#Total nonnative species
-TOT_nonnative <- TOT_species_LNU %>% 
-  filter(Native_nonnative == "non-native"|
-           Native_nonnative == "invasive non-native") %>% 
-  mutate(plot_year = paste(PlotID, year, sep="_")) %>% 
-  select(plot_year, spp) 
-
-TOT_nonnative_sum <- TOT_nonnative %>% 
-  group_by(plot_year) %>% 
-  tally() %>% 
-  dplyr::rename(nonnative_rich = n)
-
-#Total native species
-TOT_native <- TOT_species_LNU %>% 
-  filter(Native_nonnative == "native") %>% 
-  mutate(plot_year = paste(PlotID, year, sep="_")) %>% 
-  select(plot_year, spp) 
-
-TOT_native_sum <- TOT_native %>% 
-  group_by(plot_year) %>% 
-  tally() %>% 
-  dplyr::rename(native_rich = n)
-
-TOT_richness <- left_join(TOT_native_sum, TOT_nonnative_sum, by = "plot_year")
-
-TOT_richness <- TOT_richness %>% 
-  separate(plot_year, c("PlotID", "year"), sep="_") 
-
-write_csv(TOT_richness, "data/clean/LNU_Total_RICHNESS.csv")
-
-    
+write.csv(shrub_seedling_data, "data/clean/shrub_seedling_data_ALL.csv")
